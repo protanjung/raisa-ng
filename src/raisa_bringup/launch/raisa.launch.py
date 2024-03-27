d1d8caa7fff21b76efe0795de5f0694cf40aea74
Pandu Surya Tantra <pandustantra@gmail.com>
2024-03-28 00:29:16 +0700

    Add config directory to installation and update rviz configuration

--- src/raisa_bringup/launch/raisa.launch.py
+++ src/raisa_bringup/launch/raisa.launch.py
@@ -1,10 +1,19 @@
 import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import EmitEvent, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.events import Shutdown
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 
 os.environ["RCUTILS_COLORIZED_OUTPUT"] = "1"
 os.environ["RCUTILS_CONSOLE_OUTPUT_FORMAT"] = "[{severity}]: {message}"
+
+path_config = PathJoinSubstitution(
+    [get_package_share_directory("raisa_bringup"), "config"]
+)
 
 
 def generate_launch_description():
@@ -27,4 +36,27 @@ def generate_launch_description():
         respawn=True,
     )
 
-    return LaunchDescription([io_vision])
+    rviz2 = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=[
+            "-d",
+            PathJoinSubstitution([path_config, "raisa.rviz"]),
+            "--ros-args",
+            "--log-level",
+            "warn",
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            io_vision,
+            rviz2,
+            RegisterEventHandler(
+                OnProcessExit(
+                    target_action=rviz2, on_exit=[EmitEvent(event=Shutdown())]
+                )
+            ),
+        ]
+    )
